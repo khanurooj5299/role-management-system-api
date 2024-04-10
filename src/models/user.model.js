a20da614d4decb527eea3e466b63cc3177c78159
urooj khan <khanurooj5299@gmail.com>
2024-04-10 19:18:13 +0530

    Updated user model so that email and created fields cannot be changed

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,20 +1,29 @@
-import mongoose from 'mongoose';
-import rolesArray from '../../config/roles.js';
+import mongoose from "mongoose";
+import rolesArray from "../../config/roles.js";
 
 const userSchema = new mongoose.Schema({
-    email: String,
-    password: String,
-    firstName: String,
-    lastName: String,
-    role: {
-        type: String,
-        enum: rolesArray
-    },
-    category: String,
-    lastUpdated: Date,
-    created: Date
+  email: {
+    type: String,
+    required: true,
+    immutable: true,
+  },
+  password: String,
+  firstName: String,
+  lastName: String,
+  role: {
+    type: String,
+    enum: rolesArray,
+    required: true,
+  },
+  category: String,
+  lastUpdated: Date,
+  created: {
+    type: Date,
+    required: true,
+    immutable: true,
+  },
 });
 
-const UserModel = mongoose.model('users',userSchema);
+const UserModel = mongoose.model("users", userSchema);
 
 export default UserModel;
